b11fd0b8d6b0b2ce5a50b3a8924af77514ce8195
Gizem K <gzmk@users.noreply.github.com>
2015-10-05 00:20:20 -0400

    Update gk_hpc_conf.m

--- better_gloss10_fit/gk_hpc_conf.m
+++ better_gloss10_fit/gk_hpc_conf.m
@@ -35,7 +35,7 @@ InitializeRenderToolbox(true);
 
 %% Tell RenderToolbox3 where to save outputs.
 % choose Matlab's default "user folder"
-myFolder = fullfile('/scratch/gk925/brdf_fitting_all/gloss10_fit');
+myFolder = fullfile('/scratch/gk925/brdf_fitting_all/better_gloss10_fit');
 
 % or choose any folder that you want RenderToolbox3 to write to
 %myFolder = 'choose/your/output/folder';
